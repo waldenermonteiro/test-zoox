53dba3b741e87f26e13d520a2b921a47f958930e
Waldener Monteiro <jjrr019@gmail.com>
2020-03-22 12:06:49 -0300

    refactor: configurando e adicionando modulos no main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -6,10 +5,63 @@ import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+import NotifyMessage from './mixins/notify-messagen.mixin'
+import Vuelidate from 'vuelidate'
+
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(Loading)
+Vue.use(Vuelidate)
 Vue.config.productionTip = false
-
+Vue.mixin({
+  mixins: [NotifyMessage],
+  data () {
+    return {
+      atributosCarregamento: {
+        container: this.fullPage ? null : this.$refs.formContainer,
+        canCancel: true,
+        onCancel: this.onCancel
+      },
+      carregamento: this.$loading
+    }
+  },
+  methods: {
+    async skeleton (params) {
+      let loader
+      if (!params.noLoading) {
+        loader = this.$loading.show({
+          // Optional parameters
+          container: this.fullPage ? null : this.$refs.formContainer,
+          canCancel: true,
+          onCancel: this.onCancel
+        })
+      }
+      try {
+        await store.dispatch(params.urlDispatch, params.params)
+        if (params.messages) this.$setNotifySuccess(params.messages)
+        if (params.callback) params.callback()
+      } catch (errors) {
+        this.$setNotifyDanger(errors)
+      } finally {
+        loader.hide()
+      }
+    },
+    async $list (params) {
+      await this.skeleton(params)
+    },
+    async $listFilter (params) {
+      await this.skeleton(params)
+    },
+    async $createOrUpdate (params) {
+      await this.skeleton(params)
+    },
+    async $remove (params) {
+      await this.skeleton(params)
+    }
+  }
+})
 new Vue({
   router,
   store,
