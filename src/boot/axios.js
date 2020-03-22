49ce26df4f3899441dd8cbed873fde861d4074ef
Waldener Monteiro <jjrr019@gmail.com>
2020-03-22 12:01:02 -0300

    feature: adicionando e configurando axios

---
+++ src/boot/axios.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+const HTTPClient = axios.create({
+  baseURL: `${process.env.API}`,
+  headers: { 'Content-Type': 'application/json', 'Cache-Control': 'no-cache', Pragma: 'no-cache' }
+})
+
+export { HTTPClient }
