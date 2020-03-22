32975825332c294dd6f38e05bd47ca3f071c9d42
Waldener Monteiro <jjrr019@gmail.com>
2020-03-22 12:07:07 -0300

    feature: adicionando a store de estado no vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Estado from '../views/estado/store'
 
 Vue.use(Vuex)
-
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    Estado
   }
 })
