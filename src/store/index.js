338f17bfc1edae537f898507be703b82c17116f8
Waldener Monteiro <jjrr019@gmail.com>
2020-03-22 23:35:51 -0300

    feature(cidade): criando e chamando a store de cidade

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Estado from '../views/estado/store'
-
+import Cidade from '../views/cidade/store'
 Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
-    Estado
+    Estado,
+    Cidade
   }
 })
