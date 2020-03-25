8d02c9eca5beac51b0bbc0fc63a06f932a2c6536
Waldener Monteiro <jjrr019@gmail.com>
2020-03-25 10:48:21 -0300

    refactor(estado): adicionando exemplos no swagger

--- Back/app/Controllers/Swagger/StateSwagger.js
+++ Back/app/Controllers/Swagger/StateSwagger.js
@@ -1,75 +1,107 @@
-
 /**
-* @swagger
-* /api/v1/states:
-*   get:
-*     tags:
-*       - State
-*     summary: List States
-*     responses:
-*       200:
-*         example:
-*           message: Hello Guess
-*   post:
-*     tags:
-*       - State
-*     summary: Create State
-*     parameters:
-*       - name: parameters
-*         in: body
-*         type: string
-*         schema:
-*           $ref: '#/definitions/NewState'
-*     responses:
-*       200:
-*         example:
-*           message: Hello Guess
-* /api/v1/states/{id}:
-*   get:
-*     tags:
-*       - State
-*     summary: Get State by id
-*     parameters:
-*       - name: id
-*         description: Id of State
-*         in: path
-*         required: true
-*         type: integer
-*         format: "int32"
-*     responses:
-*       200:
-*         description: Send hello message
-*   put:
-*     tags:
-*       - State
-*     summary: Update State
-*     parameters:
-*       - name: id
-*         description: Id of State
-*         in: path
-*         required: true
-*         type: integer
-*         format: "int32"
-*       - name: Parameters
-*         description: parameters
-*         in: body
-*         schema:
-*           $ref: '#/definitions/NewState'
-*     responses:
-*       200:
-*         description: Send hello message
-*   delete:
-*     tags:
-*       - State
-*     summary: Delete State by id
-*     parameters:
-*       - name: id
-*         description: Id of State
-*         in: path
-*         required: true
-*         type: integer
-*         format: "int32"
-*     responses:
-*       200:
-*         description: Send hello message
-*/
+ * @swagger
+ * /api/v1/states:
+ *   get:
+ *     tags:
+ *       - State
+ *     summary: List States
+ *     responses:
+ *       200:
+ *         example:
+ *           [{
+ *              "name": "Pará",
+ *              "abbreviation": "PA"
+ *           }]
+ *   post:
+ *     tags:
+ *       - State
+ *     summary: Create State
+ *     parameters:
+ *       - name: parameters
+ *         in: body
+ *         type: string
+ *         schema:
+ *           $ref: '#/definitions/NewState'
+ *     responses:
+ *       200:
+ *         example:
+ *          {
+ *            "status": 200,
+ *            "data": {
+ *            "name": "Pará",
+ *            "abbreviation": "PA",
+ *            "created_at": "2020-03-25T04:39:55.284Z",
+ *            "updated_at": "2020-03-25T04:39:55.284Z",
+ *            "_id": "5e7ae09bc1d19f4cb6d5669c"
+ *          },
+ *          "message": "Estado Pará cadastrado(a) com sucesso"
+ *         }
+ * /api/v1/states/{id}:
+ *   get:
+ *     tags:
+ *       - State
+ *     summary: Get State by id
+ *     parameters:
+ *       - name: id
+ *         description: Id of State
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         example:
+ *          {
+ *          "data": [{
+ *            "_id": "5e7acd3b8ab18f3698f1d4f5",
+ *            "name": "Pará",
+ *            "abbreviation": "PA",
+ *            "created_at": "2020-03-25T03:17:15.221Z",
+ *            "updated_at": "2020-03-25T03:17:15.221Z",
+ *           }]
+ *          }
+ *   put:
+ *     tags:
+ *       - State
+ *     summary: Update State
+ *     parameters:
+ *       - name: id
+ *         description: Id of State
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: Parameters
+ *         description: parameters
+ *         in: body
+ *         schema:
+ *           $ref: '#/definitions/NewState'
+ *     responses:
+ *       200:
+ *         example:
+ *          {
+ *           "data": {
+ *             "_id": "5e7acd3b8ab18f3698f1d4f5",
+ *             "name": "Bahia",
+ *             "abbreviation": "BA",
+ *             "created_at": "2020-03-25T03:17:15.221Z",
+ *             "updated_at": "2020-03-25T03:31:14.698Z"
+ *           },
+ *           "message": "Estado Bahia atualizado(a) com sucesso"
+ *          }
+ *   delete:
+ *     tags:
+ *       - State
+ *     summary: Delete State by id
+ *     parameters:
+ *       - name: id
+ *         description: Id of State
+ *         in: path 
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *          example:
+ *           {
+ *            "status": 200,
+ *            "message": "Estado Pará excluído(a) com sucesso"
+ *           }
+ */
