6953bde59b795760bcf6ff60a6d2f647df9fc7e3
Waldener Monteiro <jjrr019@gmail.com>
2020-03-25 05:57:37 -0300

    refactor(cidade): alterando o nome do metodo de get by id

--- Back/app/Controllers/Http/CityController.js
+++ Back/app/Controllers/Http/CityController.js
@@ -1,21 +1,21 @@
-'use strict'
-const City = use('App/Models/City')
-const StoreCity = use('App/Validators/StoreCity')
-const BaseController = use('App/Controllers/Http/BaseController')
-const CityRepository = use('App/Repositories/CityRepository')
+"use strict";
+const City = use("App/Models/City");
+const StoreCity = use("App/Validators/StoreCity");
+const BaseController = use("App/Controllers/Http/BaseController");
+const CityRepository = use("App/Repositories/CityRepository");
 class CityController extends BaseController {
   constructor() {
-    super(City, StoreCity)
-    this.CityRepository = new CityRepository(City, StoreCity)
+    super(City, StoreCity);
+    this.CityRepository = new CityRepository(City, StoreCity);
   }
   async index({ request, response }) {
-    return await this.CityRepository.index({ request, response })
+    return await this.CityRepository.index({ request, response });
   }
-  async store({ request, response, params }) {
-    return await this.CityRepository.store({ request, response, params })
+  async show({ request, response, params }) {
+    return await this.CityRepository.show({ request, response, params });
   }
   async update({ request, response, params }) {
-    return await this.CityRepository.update({ request, response, params })
+    return await this.CityRepository.update({ request, response, params });
   }
 }
-module.exports = CityController
+module.exports = CityController;
