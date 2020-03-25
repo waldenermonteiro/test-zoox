33e3a36919a1e5e68b300aac41a9f04539e95f2d
Waldener Monteiro <jjrr019@gmail.com>
2020-03-25 17:12:58 -0300

    refactor: retirando o home do menu de rotas e adicionando um redirect

--- Front/src/components/MenuDeRotas.vue
+++ Front/src/components/MenuDeRotas.vue
@@ -3,15 +3,8 @@
     <b-card-text title="Card Title" no-body>
       <b-card-header header-tag="nav">
         <b-nav card-header tabs>
-          <b-nav-item to="/" exact exact-active-class="active"
-            >Início</b-nav-item
-          >
-          <b-nav-item to="/estados" exact exact-active-class="active"
-            >Estados</b-nav-item
-          >
-          <b-nav-item to="/cidades" exact exact-active-class="active"
-            >Cidades</b-nav-item
-          >
+          <b-nav-item to="/estados" exact exact-active-class="active">Estados</b-nav-item>
+          <b-nav-item to="/cidades" exact exact-active-class="active">Cidades</b-nav-item>
         </b-nav>
       </b-card-header>
 
