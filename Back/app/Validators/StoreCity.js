6c1d3c28a26898f83478e54566c0fa33a71b189b
Waldener Monteiro <jjrr019@gmail.com>
2020-03-25 21:01:39 -0300

    feature(cidade): criando validação unique e utilizando-a no nome da cidade

--- Back/app/Validators/StoreCity.js
+++ Back/app/Validators/StoreCity.js
@@ -1,29 +1,32 @@
-'use strict'
-const Validator = use('Validator')
-const existCustomRule = use('App/Validators/Customs/Exists')
-Validator.extend('exists', existCustomRule)
+"use strict";
+const Validator = use("Validator");
+const existCustomRule = use("App/Validators/Customs/Exists");
+const uniqueCustomRule = use("App/Validators/Customs/UpdateUnique");
+Validator.extend("exists", existCustomRule);
+Validator.extend("uniqueCustom", uniqueCustomRule);
 class StoreCity {
   get name() {
-    return 'Cidade'
+    return "Cidade";
   }
   get inputs() {
-    return ['name', 'state_id']
+    return ["name", "state_id"];
   }
   rules(cityId) {
+    cityId = cityId || false
     return {
-      name: `required|unique:cities,name|max:100`,
-      state_id: 'required|exists:states,_id'
-    }
+      name: `required|uniqueCustom:cities,name,${cityId}|max:100`,
+      state_id: "required|exists:states,_id"
+    };
   }
   get messages() {
     return {
-      'name.required': 'O campo nome é obrigatório.',
-      'name.unique': 'Já existe uma cidade com esse nome, por favor, escolha outro.',
-      'name.max': 'O campo nome aceita até 100 caracteres, por favor, tente novamente.',
-      'state_id.required': 'O campo estado é obrigatório.',
-      'state_id.exists': 'Nâo existe um estado com este id, por favor, tente novamente'
-    }
+      "name.required": "O campo nome é obrigatório.",
+      "name.uniqueCustom": "Já existe uma cidade com esse nome, por favor, escolha outro.",
+      "name.max": "O campo nome aceita até 100 caracteres, por favor, tente novamente.",
+      "state_id.required": "O campo estado é obrigatório.",
+      "state_id.exists": "Nâo existe um estado com este id, por favor, tente novamente"
+    };
   }
 }
 
-module.exports = StoreCity
+module.exports = StoreCity;
